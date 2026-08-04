a546ce7a99040607fd2c3ef82379f6c5e192971e
LiselottLotta <liselottcarlsson@hotmail.com>
2026-08-04 13:04:21 +0200

    Load portfolio case links

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.write([
   '<script src="script-base.js"><\/script>',
+  '<script src="case-links.js"><\/script>',
   '<script src="vbk-assets-1.js"><\/script>',
   '<script src="vbk-image-3.js"><\/script>',
   '<script src="vbk-image-4.js"><\/script>',
