1b3380690c6866ccf523b9a872032b4eaf08cb51
LiselottLotta <liselottcarlsson@hotmail.com>
2026-08-04 14:54:50 +0200

    Publish exact uploaded VBK carousel images

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.write([
   '<script src="script-base.js"><\/script>',
   '<script src="case-links.js"><\/script>',
-  '<script src="vbk-assets-1.js"><\/script>',
-  '<script src="vbk-image-1-exact.js?v=20260804-1335"><\/script>',
-  '<script src="vbk-image-2-exact.js?v=20260804-1335"><\/script>',
-  '<script src="vbk-image-3.js"><\/script>',
-  '<script src="vbk-image-4.js"><\/script>',
-  '<script src="vbk-assets-3.js"><\/script>',
-  '<script src="vbk-assets-4.js"><\/script>',
-  '<script src="vbk-gallery.js?v=20260804-1335"><\/script>'
+  '<script src="vbk-assets-1.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-1-exact.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-2-exact.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-3.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-4.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-assets-3.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-assets-4.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-7-exact.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-image-8-exact.js?v=20260804-1448"><\/script>',
+  '<script src="vbk-gallery.js?v=20260804-1448"><\/script>'
 ].join(''));
