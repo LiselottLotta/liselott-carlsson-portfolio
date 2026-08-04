5cbb9f8a353aeb73051d95400663ed6438ca40ef
LiselottLotta <liselottcarlsson@hotmail.com>
2026-08-04 13:04:10 +0200

    Add LinkedIn links to portfolio cases

---
+++ case-links.js
@@ -0,0 +1,8 @@
+cases.vbk.links = [
+  ["Se kundcaset", "https://www.platzer.se/sv/kundcase/vbk"],
+  ["Se LinkedIn-karusellen", "https://www.linkedin.com/posts/platzer-fastigheter-ab_vbk-om-deras-flytt-till-aria-activity-7407064083579011072-qzGY"]
+];
+
+cases.gamlestaden.links = [
+  ["Se LinkedIn-karusellen", "https://www.linkedin.com/posts/platzer-fastigheter-ab_ny-bro-ny-scen-nytt-str%C3%A5k-gamlestadens-activity-7404155885536641024-am2H"]
+];
