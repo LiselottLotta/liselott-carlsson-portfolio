f252f191a063d969ae26cc36890b11f310f7e710
LiselottLotta <liselottcarlsson@hotmail.com>
2026-08-04 13:43:14 +0200

    Load exact VBK carousel images

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.write([
   '<script src="script-base.js"><\/script>',
   '<script src="case-links.js"><\/script>',
   '<script src="vbk-assets-1.js"><\/script>',
+  '<script src="vbk-image-1-exact.js?v=20260804-1335"><\/script>',
+  '<script src="vbk-image-2-exact.js?v=20260804-1335"><\/script>',
   '<script src="vbk-image-3.js"><\/script>',
   '<script src="vbk-image-4.js"><\/script>',
   '<script src="vbk-assets-3.js"><\/script>',
   '<script src="vbk-assets-4.js"><\/script>',
-  '<script src="vbk-gallery.js"><\/script>'
+  '<script src="vbk-gallery.js?v=20260804-1335"><\/script>'
 ].join(''));
